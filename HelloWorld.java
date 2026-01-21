068ce343bd6bc8c9cf8a3bbe4595edd863706b95
langamanjb <langamanjb789@mail.sandhills.edu>
2026-01-21 11:43:22 -0500

    My First Java Program

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,1 +1,14 @@
+/*
+@author: Joshua Langaman
+@date: 1/21/26
+@purpose: My first Java program
+*/
 
+//This is my class name
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+    }
+}
