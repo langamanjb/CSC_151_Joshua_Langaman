d523916636b4a802a0b913f7add6447f09ae9cf4
langamanjb <langamanjb789@mail.sandhills.edu>
2026-02-04 10:40:34 -0500

    Mammal

---
+++ bin/src/labs/example/Mammal/Mammal.java
@@ -0,0 +1,91 @@
+package bin.src.labs.example.Mammal;
+
+public class Mammal 
+{
+    String hairColor;
+    String eyeColor;
+    double bodyTemp;
+    int age;
+    double weight;
+    boolean isAwake;
+    boolean isSitting;
+
+    public Mammal(String hairColor, String eyeColor, double bodyTemp, int age, double weight, boolean isAwake, boolean isSitting) 
+    {
+        this.hairColor = hairColor;
+        this.eyeColor = eyeColor;
+        this.bodyTemp = bodyTemp;
+        this.age = age;
+        this.weight = weight;
+        this.isAwake = isAwake;
+        this.isSitting = isSitting;
+    }
+
+
+    public void run() 
+    {
+        System.out.println("The mammal is running.");
+    }
+
+    public void eat() 
+    {
+        System.out.println("The mammal is eating.");
+    }
+    
+    public void sleep() 
+    {
+        isAwake = false;
+        System.out.println("The mammal is sleeping.");
+    }
+
+    public void scratch() 
+    {
+        System.out.println("The mammal is scratching itself.");
+    }
+
+    public void wakeUp() 
+    {
+        isAwake = true;
+        System.out.println("The mammal is awake.");
+    }
+
+    public void makingNoise()
+    {
+        System.out.println("The mammal makes a noise.");
+    }
+
+    public void breath() 
+    {
+        System.out.println("The mammal is breathing.");
+    }
+
+    public void drinkingWater() 
+    {
+        System.out.println("The mammal is drinking water.");
+    }
+
+    public void isMoving()
+    {
+        System.out.println("The mammal is moving.");
+    }
+
+    public void getMammalDetails()
+    {
+        System.out.println(this.hairColor);
+        System.out.println(this.eyeColor);
+    }
+
+    public void sit()
+    {
+        System.out.println("The animal not sitting.");
+
+        if (!isSitting) 
+        {
+            isSitting = true;
+        }
+        else 
+        {
+            System.out.println("The mammal is already sitting.");
+        }
+    }
+}
