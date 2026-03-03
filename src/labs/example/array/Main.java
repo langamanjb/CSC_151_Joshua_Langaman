f5c5930944ab7a66469c64104b41b7444c8e468b
langamanjb <langamanjb789@mail.sandhills.edu>
2026-03-03 09:03:46 -0500

    ArrayOperations

---
+++ src/labs/example/array/Main.java
@@ -0,0 +1,24 @@
+/*
+@Author: Joshua Langaman
+@Date: 3/2/26
+@Description: 1. Create a public method in your ArrayOperations Class named createNewArray() that accepts one integer argument 
+and returns void. Your method should define a new integer array with the default value of elements in the array set to the 
+parameter that is passed in. You should loop over the newly created array and for each array index/position, you should add 
+the value of the current loop count to the value of the array. After the loop is complete, you should then call a private 
+method named displayArray() that returns void and takes your previously created array as an argument. You should print out 
+to the console the following: "I created a new array and it now has " + YOUR_ARRAY_LENGTH_HERE + " items in it.". Then print 
+the following on another line:  "The array items and their values are listed below: " and print the values of the array that 
+you passed in to the method.
+
+
+*/
+
+package labs.example.array;
+
+public class Main 
+{
+    public static void main(String[] args) 
+    {
+       // ArrayOperations.createNewArray(5);
+    }
+}
