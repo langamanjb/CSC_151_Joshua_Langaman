3159b73a1ae4acf57f588597a080070320cb2d96
langamanjb <langamanjb789@mail.sandhills.edu>
2026-03-25 10:58:28 -0400

    ReadingACSVFile

---
+++ src/labs/example/fileOperations/ReadingACSVFile.java
@@ -0,0 +1,8 @@
+/*
+@author: Joshua Langaman
+@date: 3/25/26
+Description: This program demonstrates how to read a CSV file in Java. It reads the contents of a CSV file, splits each line into fields, and prints them to the console.
+*/
+
+package labs.example.fileOperations;
+
